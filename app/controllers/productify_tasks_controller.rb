de09e1f1d149ace25b1ae920f12939a448a79a00
Nathan R <nathan19@hotmail.com>
2015-05-11 10:39:56 -0700

    finished styling and fixed bugs

--- app/controllers/productify_tasks_controller.rb
+++ app/controllers/productify_tasks_controller.rb
@@ -1,39 +1,38 @@
 class ProductifyTasksController < ApplicationController
   before_action :set_productify_list
-  before_action :set_productify_task, except: [:create] 
+  before_action :set_productify_task, except: [:create]
 
-  def create 
+  def create
     @productify_task = @productify_list.productify_tasks.create(productify_task_params)
     redirect_to @productify_list
-  end  
-
-
-  def destroy 
-    if @productify_task.destroy 
-      flash[:success] = "Task was deleted"
-    else 
-      flash[:error] = "Task was not deleted"
-    end 
-    redirect_to @productify_list
-  end 
-
-  def complete 
-    @productify_task.update_attritube(:completed_at, Time.now)
-    redirect_to @productify_list, notice: "Task Completed"
-  end 
-
-  private 
-
-  def set_productify_list 
-    @productify_list = ProductifyList.find(params:productify_list_id)
   end
 
+  def destroy 
+    if @productify_task.destroy
+      flash[:sucess] = "Task was deleted."
+    else
+      flash[:error] = "Task was not deleted."
+    end
+    redirect_to @productify_list
+  end
+
+  def complete
+    @productify_task.update_attribute(:completed_at, Time.now)
+    redirect_to @productify_list, notice: "Task Completed"
+  end
+
+  private
+
+  def set_productify_list
+    @productify_list = ProductifyList.find(params[:productify_list_id])
+  end
 
   def set_productify_task
     @productify_task = @productify_list.productify_tasks.find(params[:id])
-  end 
+  end
 
-  def productify_task_parms
+  def productify_task_params
     params[:productify_task].permit(:content)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  resources :productify_lists do 
-    resources :productify_tasks do 
-      member do 
+  resources :productify_lists do
+    resources :productify_tasks do
+      member do
         patch :complete
       end
     end
-  end 
+  end
 
   root 'productify_lists#index'
-
 end
